f6b8a4bc05c13077acff7fc5ea8bf0ba6a22e04e
leoadvance <leo_advance@163.com>
2018-11-16 19:05:37 +0800

    add port parameter

--- startMJPG.sh
+++ startMJPG.sh
@@ -1,2 +1,3 @@
-./mjpg_streamer -i "./input_raspicam.so -fps 30 -q 85 -x 1280 -y 720" -o "./output_http.so -w ./www"
+# -fps frame/s -x wide -p Port number
+./mjpg_streamer -i "./input_raspicam.so -fps 30 -q 85 -x 1280 -y 720" -o "./output_http.so -p 8081 -w ./www" &
 
